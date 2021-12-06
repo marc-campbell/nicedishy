f5b20efcb5317a05385f18091ecb62172bdebae4
Marc Campbell <marc.e.campbell@gmail.com>
2021-12-06 08:35:46 -0800

    Connect device returns token

--- nicedishy/pages/connect_device.js
+++ nicedishy/pages/connect_device.js
@@ -8,6 +8,27 @@ export default function Page() {
   const [isLoading, setIsLoading] = useState(false);
   const [dishies, setDishies] = useState([]);
 
+  const fetchToken = async(dishyId) => {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/dishy/${dishyId}/token`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": Utilities.getToken(),
+        },
+      });
+
+      if (!res.ok) {
+        return;
+      }
+
+      const data = await res.json();
+      return data.token;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   const fetchDishies = async() => {
     try {
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/dishies`, {
@@ -31,7 +52,8 @@ export default function Page() {
 
   const handleCardClick = async (dishyId) => {
     // TODO get token
-    window.location.href='nicedishy://connected?token=ab'
+    const token = await fetchToken(dishyId);
+    window.location.href = `nicedishy://connected?token=${token}`;
   }
 
   useEffect( async () => {
@@ -63,12 +85,11 @@ export default function Page() {
         <div className="card card-hover" style={{width: "100%"}}>
           <div className="card-body">
             <div className="row">
-              <div className="col-5" style={{textAlign: "center"}}>
+              <div className="col-5">
                 <h2 className="card-title">{dishy.name}</h2>
-                <p className="card-text">No Data Received</p>
               </div>
               <div className="col-6" />
-              <div className="col-1" style={{textAlign: "center"}}>
+              <div className="col-1">
                 <i className="bi bi-chevron-compact-right"></i>
               </div>
             </div>
