37e96fdee70065fcc2fb2aa6150a77db2ccd2e50
Marc Campbell <marc.e.campbell@gmail.com>
2022-01-07 17:11:04 -0800

    Work on some design

--- nicedishy/components/layout.js
+++ nicedishy/components/layout.js
@@ -3,14 +3,16 @@ import Footer from './footer'
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div style={{minHeight: "100vh"}}>
       <div className="col-lg-8 mx-auto">
         <Navbar />
           <div className="py-md-5 p3">
             <main>{children}</main>
           </div>
+        </div>
+      <div style={{position: "absolute", left: "0", bottom: "0", width: "100%"}}>
         <Footer />
       </div>
-    </>
+    </div>
   )
 }
