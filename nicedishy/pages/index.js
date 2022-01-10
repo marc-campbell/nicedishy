5a5391f21b2c874bba0655db2368ebe5b503c4ce
Marc Campbell <marc.e.campbell@gmail.com>
2022-01-10 15:39:08 -0800

    fix build

--- nicedishy/pages/index.js
+++ nicedishy/pages/index.js
@@ -76,7 +76,7 @@ export default function Home() {
             <div className="container px-5">
                 <div className="row gx-5 justify-content-center">
                     <div className="col-xl-8">
-                        <div className="h2 fs-1 text-white mb-4">Woo. It's free and it's exciting.</div>
+                        <div className="h2 fs-1 text-white mb-4">Woo. It&apos;s free and it&apos;s exciting.</div>
                         <img src="assets/img/tnw-logo.svg" alt="..." style={{height: "3rem"}} />
                     </div>
                 </div>
@@ -151,8 +151,8 @@ export default function Home() {
             <div className="container px-5">
                 <div className="row gx-5 align-items-center justify-content-center justify-content-lg-between">
                     <div className="col-12 col-lg-5">
-                        <h2 className="display-4 lh-1 mb-4">Let's build the most complete dataset of dishy stats!</h2>
-                        <p className="lead fw-normal text-muted mb-5 mb-lg-0">This section is perfect for featuring some information about your application, why it was built, the problem it solves, or anything else! There's plenty of space for text here, so don't worry about writing too much.</p>
+                        <h2 className="display-4 lh-1 mb-4">Let&apos;s build the most complete dataset of dishy stats!</h2>
+                        <p className="lead fw-normal text-muted mb-5 mb-lg-0">This section is perfect for featuring some information about your application, why it was built, the problem it solves, or anything else! There&apos;s plenty of space for text here, so don&apos;t worry about writing too much.</p>
                     </div>
                     <div className="col-sm-8 col-md-6">
                         <div className="px-5 px-sm-0"><img className="img-fluid rounded-circle" src="https://source.unsplash.com/u8Jn2rzYIps/900x900" alt="..." /></div>
