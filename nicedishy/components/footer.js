0e8baf5b7326177a6a4254a4960326c76ca01f81
Marc Campbell <marc.e.campbell@gmail.com>
2022-03-07 15:33:15 -0800

    style on home page

--- nicedishy/components/footer.js
+++ nicedishy/components/footer.js
@@ -22,39 +22,39 @@ export default function Footer() {
   }
 
   return (
-    <footer className="bg-light text-center py-5">
+    <footer className="bg-purple text-center py-5">
         <div className="container px-1">
             <div className="text-black-50 small">
                 <div className="row">
 
                   <div className="col-2">
-                    <h5>Dishy Owners</h5>
+                    <h5 className="header-light">Dishy Owners</h5>
                     <ul className="nav flex-column">
-                      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Join NiceDishy</a></li>
-                      <li className="nav-item mb-2"><Link href="/compare"><a className="nav-link p-0 text-muted">Compare My Dishy</a></Link></li>
-                      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Is Everything Ok?</a></li>
+                      <li className="nav-item mb-2 nav-item-light"><a href="#" className="nav-link p-0 text-muted">Join NiceDishy</a></li>
+                      <li className="nav-item mb-2 nav-item-light"><Link href="/compare"><a className="nav-link p-0 text-muted">Compare My Dishy</a></Link></li>
+                      <li className="nav-item mb-2 nav-item-light"><a href="#" className="nav-link p-0 text-muted">Is Everything Ok?</a></li>
                     </ul>
                   </div>
 
                   <div className="col-2">
-                    <h5>Dishy Waitlisters</h5>
+                    <h5 className="header-light">Dishy Waitlisters</h5>
                     <ul className="nav flex-column">
-                      <li className="nav-item mb-2"><Link href="https://docs.nicedishy.com/prepare"><a className="nav-link p-0 text-muted">Get Prepared</a></Link></li>
+                      <li className="nav-item mb-2 nav-item-light"><Link href="https://docs.nicedishy.com/prepare"><a className="nav-link p-0 text-muted">Get Prepared</a></Link></li>
                     </ul>
                   </div>
 
                   <div className="col-2">
-                    <h5>Everyone Else</h5>
+                    <h5 className="header-light">Everyone Else</h5>
                     <ul className="nav flex-column">
-                      <li className="nav-item mb-2"><Link href={`${process.env.NEXT_PUBLIC_DASHBOARD_ENDPOINT}/d/${process.env.NEXT_PUBLIC_GRAFANA_PUBLIC_FOLDER}/the-data`}><a className="nav-link p-0 text-muted">See Some Stats</a></Link></li>
-                      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Request Data</a></li>
+                      <li className="nav-item mb-2 nav-item-light"><Link href={`${process.env.NEXT_PUBLIC_DASHBOARD_ENDPOINT}/d/${process.env.NEXT_PUBLIC_GRAFANA_PUBLIC_FOLDER}/the-data`}><a className="nav-link p-0 text-muted">See Some Stats</a></Link></li>
+                      <li className="nav-item mb-2 nav-item-light"><a href="#" className="nav-link p-0 text-muted">Request Data</a></li>
                     </ul>
                   </div>
 
                   <div className="col-4 offset-1">
                     <form>
-                      <h5>Subscribe to our newsletter</h5>
-                      <p>A semi-regular newsletter about Dishy.</p>
+                      <h5 className="header-light">Subscribe to our newsletter</h5>
+                      <p className="header-light">A semi-regular newsletter about Dishy.</p>
                       <div className="d-flex w-100 gap-2">
                         <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
                         <input value={emailAddress} onChange={e => setEmailAddress(e.target.value)} type="text" className="form-control" placeholder="Email address" />
--- nicedishy/pages/index.js
+++ nicedishy/pages/index.js
@@ -1,71 +1,69 @@
 import Link from 'next/link'
 import Footer from '../components/footer';
 import Image from 'next/image';
+import Head from 'next/head';
 
 export default function Home() {
 
   return (
     <body id="page-top">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
-            <div className="container px-5">
-                <a className="navbar-brand fw-bold" href="#page-top">
-                  <Image className="navbar-brand-logo" src="/images/nicedishy-logo.png" height="48px" width="200px" alt="NiceDishy" />
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i className="bi-list"></i>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ms-auto me-4 my-3 my-lg-0">
-                        <li className="nav-item"><Link href="https://docs.nicedishy.com"><a className="nav-link me-lg-3">Docs</a></Link></li>
-                    </ul>
-                    <button className="btn mb-2 mb-lg-0" data-bs-toggle="modal" data-bs-target="#feedbackModal">
-                        <span className="d-flex align-items-center">
-                            <i className="me-2"></i>
-                            <Link href="/login">
-                              <a><Image src="/images/btn_google_signin_dark_pressed_web@2x.png" alt="Sign in with Google" width="200px" height="50px" /></a>
-                            </Link>
-                        </span>
-                    </button>
-                </div>
-            </div>
-        </nav>
+      <Head>
+        <title>NiceDishy - The missing bits for living with Starlink</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top shadow-sm bg-purple" id="mainNav">
+        <div className="container px-5">
+          <a className="navbar-brand fw-bold" href="#page-top">
+            <Image className="navbar-brand-logo" src="/images/nicedishy-logo-white.png" height="48px" width="48px" alt="NiceDishy" />
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i className="bi-list"></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ms-auto me-4 my-3 my-lg-0">
+              <li className="nav-item-light"><Link href="https://docs.nicedishy.com"><a className="nav-link me-lg-3">Docs</a></Link></li>
+              <li className="nav-item-light"><Link href="/login"><a className="nav-link me-lg-3">Login</a></Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-        <header className="masthead">
-            <div className="container px-5">
-                <div className="row gx-5 align-items-center">
-                    <div className="col-lg-6">
-                        <div className="mb-5 mb-lg-0 text-center text-lg-start">
-                            <h1 className="display-1 lh-1 mb-3">
-                              <Image src="/images/nicedishy-word.png" height="100px" width="200px" />
-                            </h1>
-                            <p className="lead fw-normal text-muted mb-5">Continuous monitoring for your Starlink Dishy (Flatface or Squarepants), from anywhere.</p>
-                            <p className="lead fw-normal text-muted mb-5">Compare the performance of your Dishy with everyone else and even get some alerts when something changes!</p>
-                        </div>
-                        <p className="lead fw-normal text-muted mb-5">  Compare the performance of your Dishy with everyone else!</p>
-                        <div className="row">
-                          <div className="col-12">
-                            <Link href="/signup">
-                              <a>
-                                <Image className="app-badge" src="/images/btn_google_signin_dark_pressed_web@2x.png" height="50px" width="200px" alt="Get Started" />
-                              </a>
-                            </Link>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="col-12">
-                            (To get started, click the Google link)
-                          </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <div className="masthead-device-mockup">
-                            <Image src="/images/nicedishy-icon.png" height="200px" width="200px" />
-                        </div>
-                    </div>
-                </div>
+      <header className="masthead">
+        <div className="container px-5">
+          <div className="row gx-5 align-items-center">
+            <div className="col-lg-6">
+              <div className="mb-5 mb-lg-0 text-center text-lg-start">
+                <h1 className="display-1 lh-1 mb-3">
+                  NiceDishy
+                </h1>
+                <p className="lead fw-normal text-muted mb-5">Continuous monitoring for your Starlink Dishy (Flatface or Squarepants), from anywhere.</p>
+                <p className="lead fw-normal text-muted mb-5">Compare the performance of your Dishy with everyone else and even get some alerts when something changes!</p>
+              </div>
             </div>
-        </header>
+            <div className="col-lg-6">
+              <div className="masthead-device-mockup">
+                <Image src="/images/screenshot-small.png" maxwidth="550px" height="150px" width="400px" />
+              </div>
+              <div className="row mt-4">
+                <div className="col-12 text-center">
+                  <Link href="/login">
+                    <a>
+                      <Image className="app-badge" src="/images/btn_google_signin_dark_pressed_web@2x.png" height="50px" width="200px" alt="Get Started" />
+                    </a>
+                  </Link>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12 text-center">
+                  Don’t have an account? <br />
+                  <Link href="/signup">Click here to get started</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
 
         <aside className="text-center bg-gradient-primary-to-secondary">
             <div className="container px-5">
@@ -80,61 +78,56 @@ export default function Home() {
         <section id="features">
             <div className="container px-5">
                 <div className="row gx-5 align-items-center">
-                    <div className="col-lg-8 order-lg-1 mb-5 mb-lg-0">
+                    <div className="col-lg-12 order-lg-1 mb-5 mb-lg-0">
                         <div className="container-fluid px-5">
                             <div className="row gx-5">
                                 <div className="col-md-6 mb-5">
-                                    <div className="text-center">
-                                        <Image src="/images/7575373_news_events_mars_emoji.png" height="200px" width="200px" />
-                                        <h3 className="font-alt">Compare</h3>
-                                        <p className="text-muted mb-0">Compare the performance of your Dishy to folks around you or anywhere in the world</p>
+                                  <div className="row">
+                                    <div className="col-3">
+                                      <Image src="/images/7575373_news_events_mars_emoji.png" height="120px" width="120px" />
                                     </div>
+                                    <div className="col-9">
+                                      <h3 className="font-alt">Compare</h3>
+                                      <p className="text-muted mb-0">Compare the performance of your Dishy to folks around you or anywhere in the world</p>
+                                    </div>
+                                  </div>
                                 </div>
                                 <div className="col-md-6 mb-5">
-                                    <div className="text-center">
-                                        <Image src="/images/7734227_rotation_orbit_planet_mars.png" height="200px" width="200px" />
-                                        <h3 className="font-alt">Monitor</h3>
-                                        <p className="text-muted mb-0">Check on the status of your Dishy from anywhere.</p>
+                                  <div className="row">
+                                    <div className="col-3">
+                                      <Image src="/images/7734227_rotation_orbit_planet_mars.png" height="120px" width="120px" />
                                     </div>
+                                    <div className="col-9">
+                                      <h3 className="font-alt">Monitor</h3>
+                                      <p className="text-muted mb-0">Check on the status of your Dishy from anywhere. Even when not home and not using the router.</p>
+                                    </div>
+                                  </div>
                                 </div>
                             </div>
-                            <div className="row">
-                                <div className="col-md-6 mb-5 mb-md-0">
-                                    <div className="text-center">
-                                        <Image src="/images/7582125_research_explore_planet_exploration_mars_science.png" height="200px" width="200px" />
-                                        <h3 className="font-alt">Learn</h3>
-                                        <p className="text-muted mb-0">Learn what to expect and understand if Starlink is working as expected.</p>
+                            <div className="row gx-5">
+                                <div className="col-md-6 mb-5">
+                                  <div className="row">
+                                    <div className="col-3">
+                                      <Image src="/images/7582125_research_explore_planet_exploration_mars_science.png" height="120px" width="120px" />
                                     </div>
+                                    <div className="col-9">
+                                      <h3 className="font-alt">Learn</h3>
+                                      <p className="text-muted mb-0">Learn what to expect and understand if Starlink is working as expected.</p>
+                                    </div>
+                                  </div>
                                 </div>
-                                <div className="col-md-6">
-                                    <div className="text-center">
-                                        <Image src="/images/7575375_emoji_image_mars_multimedia.png" height="200px" width="200px" />
-                                        <h3 className="font-alt">Be Informed</h3>
-                                        <p className="text-muted mb-0">Get an email when something changes (firmware, speed).</p>
+                                <div className="col-md-6 mb-5">
+                                  <div className="row">
+                                    <div className="col-3">
+                                      <Image src="/images/7575375_emoji_image_mars_multimedia.png" height="120px" width="120px" />
                                     </div>
+                                    <div className="col-9">
+                                      <h3 className="font-alt">Discover</h3>
+                                      <p className="text-muted mb-0">Get an email when something changes (firmware, speed).</p>
+                                    </div>
+                                  </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 order-lg-0">
-                        <div className="features-device-mockup">
-                          <Image src="/images/screenshot-small.png" maxwidth="550px" height="150px" width="400px" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section className="bg-light">
-            <div className="container px-5">
-                <div className="row gx-5 align-items-center justify-content-center justify-content-lg-between">
-                    <div className="col-12 col-lg-5">
-                        <h2 className="display-4 lh-1 mb-4">Let&apos;s build the most complete dataset of dishy stats!</h2>
-                        <p className="lead fw-normal text-muted mb-5 mb-lg-0">This section is perfect for featuring some information about your application, why it was built, the problem it solves, or anything else! There&apos;s plenty of space for text here, so don&apos;t worry about writing too much.</p>
-                    </div>
-                    <div className="col-sm-8 col-md-6">
-                        <div className="px-5 px-sm-0">
-                          {/* <Image className="img-fluid rounded-circle" src="https://source.unsplash.com/u8Jn2rzYIps/900x900" alt="..." layout='fill' /> */}
                         </div>
                     </div>
                 </div>
