bb6469922bc15a34d06c7130137441b2e9e79fcc
Marc Campbell <marc.e.campbell@gmail.com>
2022-04-22 08:30:36 -0700

    move posthog to useeffect

--- nicedishy/pages/_app.js
+++ nicedishy/pages/_app.js
@@ -1,14 +1,14 @@
 import '../styles/globals.css'
 import '../styles/home.css'
 import posthog from 'posthog-js';
+import React, { useEffect } from 'react';
 
 const App = ({ Component, pageProps }) => {
-  console.log(typeof window);
-  if (typeof window !== "undefined") {
+  useEffect(() => {
     if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {api_host: 'https://app.posthog.com'})
     }
-  }
+  }, []);
 
   const getLayout = Component.getLayout || ((page) => page);
   return getLayout(<Component {...pageProps} />);
