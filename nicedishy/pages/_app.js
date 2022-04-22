455aa9e803d110f4aa0fac67e4f7d78e8339264b
Marc Campbell <marc.e.campbell@gmail.com>
2022-04-22 08:11:03 -0700

    posthog prerender

--- nicedishy/pages/_app.js
+++ nicedishy/pages/_app.js
@@ -3,8 +3,11 @@ import '../styles/home.css'
 import posthog from 'posthog-js';
 
 const App = ({ Component, pageProps }) => {
-  if (typeof window !== undefined && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {api_host: 'https://app.posthog.com'})
+  console.log(typeof window);
+  if (typeof window !== "undefined") {
+    if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {api_host: 'https://app.posthog.com'})
+    }
   }
 
   const getLayout = Component.getLayout || ((page) => page);
--- nicedishy/pages/signup/callback.js
+++ nicedishy/pages/signup/callback.js
@@ -46,7 +46,7 @@ function LoginCallback() {
           return;
         }
 
-        if (typeof window !== undefined && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+        if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
           posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
             api_host: 'https://app.posthog.com',
             loaded: function(posthog) {
