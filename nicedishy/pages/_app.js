80424cfb308ddb96301f93490dd31a3ce79394a5
Marc Campbell <marc.e.campbell@gmail.com>
2022-04-22 08:42:08 -0700

    remove

--- nicedishy/pages/_app.js
+++ nicedishy/pages/_app.js
@@ -1,14 +1,14 @@
 import '../styles/globals.css'
 import '../styles/home.css'
-import posthog from 'posthog-js';
+// import posthog from 'posthog-js';
 import React, { useEffect } from 'react';
 
 const App = ({ Component, pageProps }) => {
-  useEffect(() => {
-    if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {api_host: 'https://app.posthog.com'})
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  //     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {api_host: 'https://app.posthog.com'})
+  //   }
+  // }, []);
 
   const getLayout = Component.getLayout || ((page) => page);
   return getLayout(<Component {...pageProps} />);
