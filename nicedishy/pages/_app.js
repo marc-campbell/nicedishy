8739b35fd38e918b4fcc0f305718411c70ef839c
Marc Campbell <marc.e.campbell@gmail.com>
2022-04-22 07:19:40 -0700

    avoid server side render

--- nicedishy/pages/_app.js
+++ nicedishy/pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/home.css'
 import posthog from 'posthog-js';
 
 const App = ({ Component, pageProps }) => {
-  if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  if (typeof window !== undefined && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {api_host: 'https://app.posthog.com'})
   }
 
--- nicedishy/pages/login/callback.js
+++ nicedishy/pages/login/callback.js
@@ -45,7 +45,7 @@ function LoginCallback() {
         return;
       }
 
-      if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+      if (typeof window !== undefined && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
           api_host: 'https://app.posthog.com',
           loaded: function(posthog) {
